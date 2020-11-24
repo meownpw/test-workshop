87b1f0a4ee6e0d25c58c1dc8f6293b7dc453183d
meownpw <36756656+meownpw@users.noreply.github.com>
2020-11-24 15:57:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="App">
           <header className="App-header">
             <img src={'https://career.mfec.co.th/Career/img/mfec_logo.png'} className="App-logo" alt="logo" />
-            <p> meowmeow</p>
+            <p> meowmeowmeow</p>
             <p >{'MFEC CLOUD SERVICES'}</p>
           </header>
         </div>
